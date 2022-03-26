07b93c2f70dfa9c266be5f10a45008f32058ee99
Caique Santos <caiquebsan@gmail.com>
2022-03-25 21:10:08 -0300

    Inserção pagina de exemplo html

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-        return ( < div > Meu primeiro componente React. < /div>)
+        return ( 
+                <div> 
+                        Meu primeiro componente React. 
+                </div>
+                )
         }
 
-        ReactDOM.render( < App / > ,
-            document.querySelector("#root"))
+ReactDOM.render( <App/> ,
+        document.querySelector("#root")
+)
